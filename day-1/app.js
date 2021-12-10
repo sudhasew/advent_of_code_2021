7ea9420d702646b55bd5d80e62694b3294b57947
sudhasew <sudha.sew@gmail.com>
2021-12-10 10:27:16 -0500

    first part is done

--- day-1/app.js
+++ day-1/app.js
@@ -1,6 +1,19 @@
 const fs = require("fs");
-const depths = fs.readFileSync("input.txt").toString().split("\n");
-
-for (let i in depths) {
-  console.log(depths[i]);
+const depths = fs
+  .readFileSync("input.txt", { encoding: "utf-8" })
+  .split("\n")
+  .filter((x) => Boolean(x))
+  .map((x) => parseInt(x));
+let counter = 0;
+for (let i = 1; i < depths.length; i++) {
+  const last = depths[i - 1];
+  const current = depths[i];
+  if (current > last) {
+    counter++;
+  }
 }
+console.log(counter);
+
+// for (let i in depths) {
+//   console.log(depths[i]);
+// }
