ac7b5520a94f15a274fd661c65b00c4ecc42bc56
sudhasew <sudha.sew@gmail.com>
2021-12-10 15:21:08 -0500

    completed day1

--- day-1/app.js
+++ day-1/app.js
@@ -11,3 +11,14 @@ for (let i = 1; i < depths.length; i++) {
   }
 }
 console.log(counter);
+
+let counter2 = 0;
+for (let i = 3; i < depths.length; i++) {
+  if (
+    depths[i] + depths[i - 1] + depths[i - 2] >
+    depths[i - 1] + depths[i - 2] + depths[i - 3]
+  ) {
+    counter2++;
+  }
+}
+console.log(counter2);
