c8aa03c7c6d6566939e253903903742b160bde32
sudhasew <sudha.sew@gmail.com>
2021-12-11 18:52:38 -0500

    final commit

--- day-2/day2.js
+++ day-2/day2.js
@@ -1,36 +1,36 @@
 const fs = require("fs");
-const { resourceLimits } = require("worker_threads");
+//const { resourceLimits } = require("worker_threads");
 const depths = fs.readFileSync("input.txt").toString().split("\n");
 
-// let x = 0;
-// let y = 0;
+//PART ONE
+let x = 0;
+let y = 0;
 
-// for (let i = 0; i < depths.length; i++) {
-//   let command = depths[i];
-//   let inputCommand = command.split(" ");
-//   let input = inputCommand[0];
-//   num = inputCommand[1];
-//   console.log(input);
-//   console.log(num);
-//   if (input == "forward") {
-//     x = x + parseInt(num);
-//     y = y;
-//     console.log(x, y);
-//   } else if (input == "down") {
-//     x = x;
-//     y = y - parseInt(num);
-//     console.log(x, y);
-//   } else if (input == "up") {
-//     x = x;
-//     y = y + parseInt(num);
-//     console.log(x, y);
-//   }
-// }
-// console.log(Math.abs(x));
-// console.log(Math.abs(y));
-// let z = Math.abs(x) * Math.abs(y);
-// console.log(z);
+for (let i = 0; i < depths.length; i++) {
+  let command = depths[i];
+  let inputCommand = command.split(" ");
+  let input = inputCommand[0];
+  num = inputCommand[1];
+  if (input == "forward") {
+    x = x + parseInt(num);
+    y = y;
+    console.log("forwardOne", x, y);
+  } else if (input == "down") {
+    x = x;
+    y = y - parseInt(num);
+    console.log("downOne", x, y);
+  } else if (input == "up") {
+    x = x;
+    y = y + parseInt(num);
+    console.log("upOne", x, y);
+  }
+}
+console.log(Math.abs(x));
+console.log(Math.abs(y));
+let z = Math.abs(x) * Math.abs(y);
+console.log(z);
 
+// PART TWO
 let x2 = 0;
 let aim = 0;
 let depth = 0;
